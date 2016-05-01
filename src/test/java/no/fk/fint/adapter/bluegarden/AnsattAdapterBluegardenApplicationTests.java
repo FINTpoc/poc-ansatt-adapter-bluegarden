0cfde7456d860d36f8118d876114ab602ddcb62b
Frode Sjovatsen <frode.sjovatsen@rogfk.no>
2016-05-01 19:52:57 +0200

    initial commit

---
+++ src/test/java/no/fk/fint/adapter/bluegarden/AnsattAdapterBluegardenApplicationTests.java
@@ -0,0 +1,18 @@
+package no.fk.fint.adapter.bluegarden;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class AnsattAdapterBluegardenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
